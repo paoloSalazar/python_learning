29c0731e28aa05b4553f8dfdf5f162381dac8653
paoloSalazar <sid.philips6189@gmail.com>
2025-01-08 10:41:31 +0000

    data classes definition

---
+++ POO_and_Classes/lesson7/poo.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+
+@dataclass
+class Project:
+    """data clases are used to avoid writting all class init method"""
+    name: str
+    payment: int
+    client: str
+    
+    
+
+# class Project:
+#     def __init__(self, name, payment, client):
+#         self.name = name
+#         self.payment = payment
+#         self.client = client
+
+class Employee:
+    def __init__(self, name, age, salary, project):
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.project = project
+
+
+p = Project("Django APP", 20000, "Globomatics")
+e = Employee("Ji-Soo", 38, 1000, p)
+print(e.project)
