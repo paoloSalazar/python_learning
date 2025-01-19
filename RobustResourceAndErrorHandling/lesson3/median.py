731267831133896f4f2361fb3334899ac77dd8d3
Paolo Salazar <paolo.salazar@jalasoft.com>
2025-01-19 01:50:34 -0400

    Python Exception handling

---
+++ RobustResourceAndErrorHandling/lesson3/median.py
@@ -0,0 +1,18 @@
+
+def median(iterable):
+    items = sorted(iterable)
+
+    if len(items) == 0:
+        raise ValueError("median() arg is an empty series")
+
+    median_index = (len(items) -1) // 2
+    if len(items) % 2 != 0:
+        return items[median_index]
+    return (items[median_index] + items[median_index + 1]) / 2
+
+def main():
+    try:
+        median([])
+    except ValueError as e:
+        print("Payload repr:", repr(e))
+        print("Payload str:", str(e))
