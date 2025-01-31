86b94c54cc5c4c698f4f09b41c37344e03ca86cb
paoloSalazar <sid.philips6189@gmail.com>
2025-01-31 11:11:46 +0000

    Context Manager Decorator propagate exception

--- RobustResourceAndErrorHandling/lesson10/cm.py
+++ RobustResourceAndErrorHandling/lesson10/cm.py
@@ -9,3 +9,4 @@ def logging_context_manager():
         print('logging_context_manager: normal exit')
     except Exception:
         print('logging_context_manager: exceptional exit', sys.exc_info())
+        raise
