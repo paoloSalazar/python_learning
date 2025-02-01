eb13769a94d894b7612476c7bbd4f473955841a2
paoloSalazar <sid.philips6189@gmail.com>
2025-02-01 11:26:48 +0000

    add propagater function

--- RobustResourceAndErrorHandling/lesson11/cm.py
+++ RobustResourceAndErrorHandling/lesson11/cm.py
@@ -5,3 +5,14 @@ def nest_test(name):
     print("Entering", name)
     yield name
     print("exiting", name)
+
+
+@contextlib.contextmanager
+def propagater(name, propagate):
+    try:
+        yield
+        print(name, 'exited normally.')
+    except Exception:
+        print(name, 'received an exception')
+        if propagate:
+            raise
