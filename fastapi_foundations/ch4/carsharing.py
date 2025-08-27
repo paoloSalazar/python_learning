1bc4ba080b2438d53dd030d38e3f201f08796e3a
paoloSalazar <sid.philips6189@gmail.com>
2025-08-27 00:28:41 +0000

    chapter 4 fastapi

---
+++ fastapi_foundations/ch4/carsharing.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, HTTPException
+import uvicorn
+
+app = FastAPI()
+
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
+
+# @app.get("/api/cars")
+# def get_cars(size: str = None):
+#     if size:
+#         return [car for car in db if car["size"] == size]
+#     return db
+
+@app.get("/api/cars")
+def get_cars(size: str|None = None, doors: int|None =None) -> list:
+    result = db
+    if size:
+        result = [car for car in db if car["size"] == size]
+    if doors:
+        result = [car for car in result if car["doors"] >= doors]
+    return result
+
+@app.get("/api/cars/{car_id}")
+def get_car_by_id(car_id: int) -> dict:
+    result = [car for car in db if car["id"] == car_id]
+    if result:
+        return result[0]
+    else:
+        raise HTTPException(status_code=404, detail="Car not found")
+
+if __name__ == "__main__":
+    uvicorn.run("carsharing:app", host="0.0.0.0", port=8000, reload=True)
